c4a9547407c78c6bcce09a4a6b0de505b1b265ff
mareena <mereena333@gmail.com>
2024-10-03 02:15:46 +0530

    first commit

---
+++ client/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h2 className="home-header">Home Component</h2>
+      <p className="home-content">Welcome to our home page!</p>
+    </div>
+  );
+}
+
+export default Home;
